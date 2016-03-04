9835eba8ebc1834da401481bb99e546ed49c43ce
toptest09 <patrikkulacs18@gmail.com>
2016-03-04 23:47:02 +0100

    Leafe

---
+++ Leafe/app/src/main/java/upom.java
@@ -0,0 +1,24 @@
+/**
+ * Created by L on 28.2.2016.
+ */
+package com.android4devs.slidingtab;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class Tab2 extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v =inflater.inflate(R.layout.tab_2,container,false);
+        return v;
+    }
+}
+
+/*public class upom {
+}*/
